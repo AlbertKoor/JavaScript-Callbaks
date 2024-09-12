31af9ab3153a81c592277a87588acebdd4575881
Albert Kooro <albertkoori12@gmail.com>
2024-09-12 15:38:44 -0300

    AddFunctions

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ ShowItems(letras, fazConsoleLog);
 /*ShowItems(letras, function(item){
 console.log(item)
 });*/ 
+
+function somar(num1, num2) {
+  return num1 + num2;
+}
+
+function imprimirResultado(resultado) {
+  console.log("O resultado da soma é: " + resultado);
+}
+
+function somar(num1, num2, callback) {
+  let resultado = num1 + num2;
+  callback(resultado);
+}
+
+somar(2, 3, imprimirResultado);
